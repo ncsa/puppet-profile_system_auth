57aaf5c8d299f187aa61f96e323f6f8a92a72a28
Bill Glick <bglick@illinois.edu>
2022-09-09 10:24:32 -0500

    SECURITY-1502: Add ability to disable kerberos

--- manifests/kerberos.pp
+++ manifests/kerberos.pp
@@ -14,6 +14,9 @@
 # @param crons
 #   Hash of cron resource parameters for any CRON entries related to kerberos keytab cleanup
 #
+# @param enable
+#   Used to enable or disable kerberos
+#
 # @param files_remove_setuid
 #   Hash of file resource paramters that need setuid removed from them
 #
@@ -31,88 +34,92 @@ class profile_system_auth::kerberos (
   Optional[ String ] $createhostkeytab,  # BASE64 ENCODING OF KRB5 CREATEHOST KEYTAB FILE
   Optional[ String ] $createhostuser,    # CREATEHOST USER
   Hash               $crons,
+  Boolean            $enable,
   Hash               $files_remove_setuid,
   Array[ String[1] ] $required_pkgs,     # DEFAULT SET VIA MODULE DATA
   Optional[ Array[ String[1] ] ] $root_k5login_principals, # PRINCIPALS WITH ROOT PRIVILEGES
 ) {
 
-  ensure_packages( $required_pkgs )
+  if ($enable) {
 
-  File {
-    owner  => root,
-    group  => root,
-    ensure => file,
-    mode   => '0644',
-  }
+    ensure_packages( $required_pkgs )
 
-  # Remove setuid/setgid from key files
-  $file_remove_setuid_defaults = {
-    mode    => 'ug-s',
-  }
-  ensure_resources('file', $files_remove_setuid, $file_remove_setuid_defaults )
+    File {
+      owner  => root,
+      group  => root,
+      ensure => file,
+      mode   => '0644',
+    }
 
-  # Ensure directory for include files
-  file { '/etc/krb5.conf.d':
+    # Remove setuid/setgid from key files
+    $file_remove_setuid_defaults = {
+      mode    => 'ug-s',
+    }
+    ensure_resources('file', $files_remove_setuid, $file_remove_setuid_defaults )
+
+    # Ensure directory for include files
+    file { '/etc/krb5.conf.d':
       ensure => directory,
       mode   => '0755',
-  }
-
-  # CREATE ALL THE FILES LISTED IN THE cfg_file_settings HASH
-  $cfg_file_settings.each | $filename, $content | {
-    file { $filename:
-      content => $content,
-    }
-  }
-
-  if ( $root_k5login_principals )
-  {
-    file { '/root/.k5login':
-      content => join($root_k5login_principals, "\n"),
-      mode    => '0600',
-    }
-  }
-  else
-  {
-    file { '/root/.k5login':
-      ensure => 'absent',
-    }
-  }
-
-  if ( $createhostkeytab and $createhostuser )
-  {
-    # CREATE KEYS AND SETUP RENEWAL
-    file { '/root/createhostkeytab.sh':
-      ensure => present,
-      mode   => '0700',
-      source => "puppet:///modules/${module_name}/root/createhostkeytab.sh",
-    }
-    ## THIS MIGHT NEED TO BE SMARTER TO ALLOW FOR MULTIPLE HOSTNAMES ON ONE SERVER
-    exec { 'create_host_keytab':
-      path    => [ '/usr/bin', '/usr/sbin'],
-      command => "/root/createhostkeytab.sh ${createhostkeytab} ${createhostuser}",
-      unless  => 'klist -kt /etc/krb5.keytab 2>&1 | grep "host/`hostname -f`@NCSA.EDU"',
-      require => [
-        File[ '/root/createhostkeytab.sh' ],
-      ]
     }
 
-    Cron {
-      user        => 'root',
-      hour        => 8,
-      minute      => 1,
-      environment => ['SHELL=/bin/sh', ],
+    # CREATE ALL THE FILES LISTED IN THE cfg_file_settings HASH
+    $cfg_file_settings.each | $filename, $content | {
+      file { $filename:
+        content => $content,
+      }
     }
-    $crons.each | $k, $v | {
-      cron { $k: * => $v }
+
+    if ( $root_k5login_principals )
+    {
+      file { '/root/.k5login':
+        content => join($root_k5login_principals, "\n"),
+        mode    => '0600',
+      }
     }
-  }
-  else
-  {
-    file { '/root/createhostkeytab.sh':
-      ensure => absent,
+    else
+    {
+      file { '/root/.k5login':
+        ensure => 'absent',
+      }
     }
-    $crons.each | $k, $v | {
-      cron { $k: ensure => absent, }
+
+    if ( $createhostkeytab and $createhostuser )
+    {
+      # CREATE KEYS AND SETUP RENEWAL
+      file { '/root/createhostkeytab.sh':
+        ensure => present,
+        mode   => '0700',
+        source => "puppet:///modules/${module_name}/root/createhostkeytab.sh",
+      }
+      ## THIS MIGHT NEED TO BE SMARTER TO ALLOW FOR MULTIPLE HOSTNAMES ON ONE SERVER
+      exec { 'create_host_keytab':
+        path    => [ '/usr/bin', '/usr/sbin'],
+        command => "/root/createhostkeytab.sh ${createhostkeytab} ${createhostuser}",
+        unless  => 'klist -kt /etc/krb5.keytab 2>&1 | grep "host/`hostname -f`@NCSA.EDU"',
+        require => [
+          File[ '/root/createhostkeytab.sh' ],
+        ]
+      }
+
+      Cron {
+        user        => 'root',
+        hour        => 8,
+        minute      => 1,
+        environment => ['SHELL=/bin/sh', ],
+      }
+      $crons.each | $k, $v | {
+        cron { $k: * => $v }
+      }
+    }
+    else
+    {
+      file { '/root/createhostkeytab.sh':
+        ensure => absent,
+      }
+      $crons.each | $k, $v | {
+        cron { $k: ensure => absent, }
+      }
     }
   }
 }
